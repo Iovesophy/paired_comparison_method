9f34293ec3b9b5f6b6da3367353d1ae35b8db530
Odenn <kazuya@odenn.org>
2021-01-10 22:31:49 +0900

    repair:change itertools error

---
+++ analyzer/statistics_NormalDist_sample.py
@@ -0,0 +1,14 @@
+from statistics import NormalDist
+
+sum_val = 14
+real_data = 9
+
+data_calculation = float(real_data/sum_val)
+
+print(data_calculation)
+
+#NormalDist(mu=10, sigma=2).inv_cdf(0.95)
+# パラメータ指定がないと、標準正規分布(mu = 0およびsigma = 1)に自動補完される。
+print(NormalDist().inv_cdf(data_calculation))
+
+
--- selector/select_main.py
+++ selector/select_main.py
@@ -67,7 +67,7 @@ def material_get(n): # 試料読み込み
 
 def itertools_make_material(material): # 試行回数分一対比較用の比較試料生成
     itertools_material=[]
-    for i in itertools.combinations_count(material, r=2):
+    for i in itertools.combinations(material, r=2):
         itertools_material.append(i)
     random.shuffle(itertools_material)
     return itertools_material
