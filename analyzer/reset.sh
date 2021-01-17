5cc3d535329c920c7d5bf3b7b4a30c4dc4b9a904
Odenn <kazuya@odenn.org>
2021-01-17 15:26:12 +0900

    push:add folder

--- analyzer/reset.sh
+++ analyzer/reset.sh
@@ -4,3 +4,4 @@ rm ./plot_data/*
 rm ./internal_consistency_data/*
 rm ./n.txt
 touch ./n.txt
+touch ./view_data/.info.txt ./analyze_data/.info.txt ./plot_data/.info.txt ./internal_consistency_data/.info.txt
